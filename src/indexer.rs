aa66f51e552a16cdeee4f5db0b33b1f1c5fcae90
YsfHam <ysf.hamdouni@gmail.com>
2023-08-28 15:45:44 +0200

    Use cosine similarity for ranking

--- src/indexer.rs
+++ src/indexer.rs
@@ -31,26 +31,71 @@ pub struct Indexer {
     doc_freq: HashMap<String, f32>,
 }
 
+fn compute_cos_sim(a: &Vec<f32>, b: &Vec<f32>) -> f32 {
+    let norm_a_sq = compute_dot_product(a, a).sqrt();
+    let norm_b_sq = compute_dot_product(b, b).sqrt();
+
+    let dot_a_b = compute_dot_product(a, b);
+
+
+    dot_a_b / (norm_a_sq * norm_b_sq)
+}
+
+fn compute_dot_product(a: &Vec<f32>, b: &Vec<f32>) -> f32 {
+    a.iter().zip(b.iter()).map(|(a, b)| a * b).sum()
+}
+
 impl Indexer {
     pub fn get_docs_for_query(&self, query: &str) -> Vec<(PathBuf, f32)> {
 
-        let mut docs_score = self.documents.keys()
-            .map(|doc| (doc.to_owned(), self.compute_tf_idf_query(doc, query)))
+        let idfs = self.compute_idfs_query(query);
+        let tf_idf_query = self.compute_tf_idf_query(query, &idfs);
+
+        let mut docs = self.documents.keys()
+            .map(|doc| {
+                let tf_idf = self.compute_tf_idf_query_doc(doc, query, &idfs);
+                (doc.to_owned(), compute_cos_sim(&tf_idf, &tf_idf_query))
+            })
+            .filter(|(_, v)| !v.is_nan())
             .collect::<Vec<_>>();
         
-        docs_score.sort_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap());
-        docs_score.reverse();
-        docs_score
+
+        docs.sort_by(|(doc_a, a), (doc_b, b)|{
+            let ordering = b.partial_cmp(a).unwrap();
+            if ordering.is_eq() {
+                doc_a.to_str().unwrap().cmp(doc_b.to_str().unwrap())
+            }
+            else {
+                ordering
+            }
+        });
+
+        docs
+
     }
 
-    fn compute_tf_idf_term(&self, doc: &PathBuf, term: &String) -> f32 {
-        let tf = self.documents.get(doc)
-                                            .unwrap()
-                                            .tfs
-                                            .get(term)
-                                            .and_then(|x| Some(*x))
-                                            .unwrap_or(0.0);
+    fn compute_tf_idf_term(&self, doc: &PathBuf, term: &String, idf: f32) -> f32 {
+        let tf = self.documents
+                        .get(doc)
+                        .unwrap()
+                        .tfs
+                        .get(term)
+                        .and_then(|x| Some(*x))
+                        .unwrap_or(0.0);
 
+        tf * idf
+
+    }
+
+    fn compute_tf_idf_query_doc(&self, doc: &PathBuf, query: &str, idfs: &HashMap<String, f32>) -> Vec<f32> {
+        Lexer::new(query)
+            .map(|term| self.compute_tf_idf_term(
+                doc, &term, 
+                idfs.get(&term).and_then(|x| Some(*x)).unwrap()))
+            .collect()
+    }
+
+    fn compute_idf_term(&self, term: &str) -> f32 {
         let docs_number = self.documents.len() as f32;
 
         let docs_number_for_term = 
@@ -58,17 +103,50 @@ impl Indexer {
                     .and_then(|x| Some(*x))
                     .unwrap_or(1.0);
 
-        let idf = (docs_number / docs_number_for_term).log10();
-
-        tf * idf
-
+        (docs_number / docs_number_for_term).log10()
     }
 
-    fn compute_tf_idf_query(&self, doc: &PathBuf, query: &str) -> f32 {
-        let lexer = Lexer::new(query);
-
-        lexer.map(|term| self.compute_tf_idf_term(doc, &term)).sum()
+    fn compute_idfs_query(&self, query: &str) -> HashMap<String, f32> {
+        Lexer::new(query)
+            .map(|term| {
+                let idf_term = self.compute_idf_term(&term);
+                (term, idf_term)
+            })
+            .collect()
     }
+
+    fn compute_tf_idf_query(&self, query: &str, idfs: &HashMap<String, f32>) -> Vec<f32> {
+
+        let tfs = compute_tfs(query);
+
+        Lexer::new(query)
+        .map(|term| {
+
+            let tf = tfs.get(&term).unwrap();
+            let idf = idfs.get(&term).unwrap();
+            
+            tf * idf
+        })
+        .collect()
+    }
+}
+
+
+fn compute_tfs(content: &str) -> HashMap<String, f32> {
+    let mut terms_freqs = HashMap::new();
+    for term in Lexer::new(&content) {
+        if let Some(freq) = terms_freqs.get_mut(&term) {
+            *freq += 1.0;
+        }
+        else {
+            terms_freqs.insert(term, 1.0);
+        }
+    }
+
+    let sum = terms_freqs.values().sum::<f32>();
+    terms_freqs.values_mut().for_each(|v| *v /= sum);
+
+    terms_freqs
 }
 
 
@@ -80,18 +158,7 @@ where
     let file_content = read_file(file_path)?;
     // tokenize the file into terms
     // for each term compute terms frequency
-    let mut terms_freqs = HashMap::new();
-    for term in Lexer::new(&file_content) {
-        if let Some(freq) = terms_freqs.get_mut(&term) {
-            *freq += 1.0;
-        }
-        else {
-            terms_freqs.insert(term, 1.0);
-        }
-    }
-
-    let sum = terms_freqs.values().sum::<f32>();
-    terms_freqs.values_mut().for_each(|v| *v /= sum);
+    let terms_freqs = compute_tfs(&file_content);
 
     // compute update document requency for each term
 
